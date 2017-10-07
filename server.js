b22f9b55367598fe192a7c3d71f7a78f924eb42a
Krit Petrachaianan <krit.petra@gmail.com>
2017-10-07 15:22:47 -0700

    added query options to backend data api

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const fs = require("fs");
 const port = process.env.PORT || 3001;
 const utilData = JSON.parse(fs.readFileSync('data/utilData.json', 'utf8'));
 
+/*
+ *  GET /data?q=[column] => [{ month: Date, [column]: [data] }, ...]
+ */
 app.get("/data", (req, res) => {
-  res.json(utilData);
+  if (!req.query.q) {
+    res.json(utilData); // No query specified -- get unaltered dataset
+  } else {
+    res.json(utilData.map(entry =>
+      [Date.UTC(entry.year, entry.month), entry[req.query.q]]
+    ));
+  }
 });
 
 // Possible to establish more routes/queries for data preprocessing --
